34b05aaf3f43d8f5e28f440e15d60b90652267d0
Ignacio Fuertes <ifbyol@gmail.com>
2012-10-10 10:25:02 +0200

    Some errors in package "stories" were repaired

--- src/test/java/org/weso/wesearch/stories/FindLocationStory.java
+++ src/test/java/org/weso/wesearch/stories/FindLocationStory.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.weso.wesearch.stories;
 
 import java.util.List;
@@ -29,35 +28,3 @@ public class FindLocationStory extends JUnitStory {
 	}
 
 }
-=======
-package org.weso.wesearch.stories;
-
-import java.util.List;
-
-import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.io.LoadFromClasspath;
-import org.jbehave.core.junit.JUnitStory;
-import org.jbehave.core.reporters.Format;
-import org.jbehave.core.reporters.StoryReporterBuilder;
-import org.jbehave.core.steps.CandidateSteps;
-import org.jbehave.core.steps.InstanceStepsFactory;
-import org.weso.wesearch.steps.FindLocation;
-
-public class FindLocationStory extends JUnitStory {
-	
-	@Override
-	public Configuration configuration() {
-		return new MostUsefulConfiguration().useStoryLoader(
-				new LoadFromClasspath(this.getClass())).useStoryReporterBuilder(
-						new StoryReporterBuilder().withFormats(Format.HTML, Format.CONSOLE));
-	}
-	
-	@Override
-	public List<CandidateSteps> candidateSteps() {
-		return new InstanceStepsFactory(configuration(), new FindLocation())
-        	.createCandidateSteps();
-	}
-
-}
->>>>>>> b154fd00cbd1ca6d079ecc5007b2efb9514620bf
